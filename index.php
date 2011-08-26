45340c7eb40730fd2b999c2d96ef36b5a3b804a1
Scott Nixon <citadelgrad@gmail.com>
2011-08-25 21:53:31 -0400

    Working form submissions buttons in twitter stream.

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <head>
 	<title>Learn PHP & MongoDB</title>
 <link rel="stylesheet" href="http://css.happyherbivore.com/960test/960.css" />
+<script type="text/javascript" 
+  src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 <style>
 p.tweets {
 	 overflow: hidden;
@@ -14,6 +16,23 @@ p.tweets {
 p.tweets span {float:right;}
 
 </style>
+<script>
+$(document).ready(function(){
+
+    $('.add').click( function () {
+        $.ajax({
+           url: '/',
+	   type: 'POST',
+	   success: function(result){
+	       location.reload(true);
+	   }
+	   });
+    
+    });
+
+});
+</script>
+
 </head>
 <body>
 <div class="container_12">
@@ -21,16 +40,53 @@ p.tweets span {float:right;}
 
      <h2>MongoDB records</h2>
 <?php
-
 ini_set ('display_errors', 1);
 
 error_reporting (E_ALL | E_STRICT);
 
+//$your_input = '';
+//$your_input = $_POST['input'];
+?>
 
-$your_input = '';
-$your_input = $_POST['input'];
+<?php
+if ($_POST) {
+
+   try {
+	// open connection to MongoDB server
+	$conn = new Mongo('localhost');
+
+	// access database
+	$db = $conn->test;
+
+	// access collection
+	$collection = $db->items;
+
+	// execute query to retrieve all documents
+	//$cursor = $collection->find();
+
+	//$collection->insert($someDoc);
+
+	echo "POST " . $_POST['tweetitem'] . "<br>";
+
+	$conn->close();
+   } catch (MongoConectionException$e) {
+	die('Error connecting to MongoDB server');
+   } catch (MongoException$e) {
+	die('Error: ' .$e->getMessage());
+  }
+
+}  
+
+$lines = file_get_contents('stream.json');
+$grp = json_decode($lines, true);
+
+$entry = $grp;
+
+echo $entry[1]['text'];
 
 ?>
+
+
 
 <form action="." method="POST">
 <input name="input" type="text">
@@ -58,8 +114,8 @@ try {
 	echo $cursor -> count() .' document(s) found. <br>';
 	foreach ($cursor as $obj) {
 		echo 'Name: ' .$obj['name'] .'<br>';
-		echo 'Quantity: ' .$obj['quanity'] .'<br>';
-		echo 'Price: ' .$obj['price'] .'<br>';
+		echo 'Photo: ' .$obj['quanity'] .'<br>';
+		echo 'text: ' .$obj['price'] .'<br>';
 		echo '<br>';
 	}
 
@@ -75,6 +131,9 @@ try {
 
 <div class="grid_6">
  <?php
+// Great starting reference for this example.
+// http://webhole.net/2009/08/31/how-to-read-json-data-with-php/
+
 // Make a string object
 $lines = file_get_contents('stream.json');
 
@@ -93,10 +152,13 @@ $grp = json_decode($lines);
 <?php
 
 // Display Twitter Stream for a based on USERNAME
-foreach ($grp as $json) {
+foreach ($grp as $key => $json) {
 	echo "<p class='tweets'><span>" .$json->user->screen_name;
 	echo "<img src='" .$json->user->profile_image_url ."'></span>";
-	echo $json->text . "</p>";
+	echo $json->text; 
+	echo "<form action='.' method=POST>";
+	echo "<input name='tweetitem' value='$key' type='hidden'>";
+	echo "<input type='submit' value='Add'></form></p>";
 }
 
 // source: http://de.php.net/json_last_error
